df70fd54f57c54f8e95ddf93008bb5b386f9a990
Kongphop1 <kongphop.t@mail.kmutt.ac.th>
2022-04-01 01:58:55 +0700

    start_again

--- New folder/Core/Src/main.c
+++ New folder/Core/Src/main.c
@@ -48,6 +48,7 @@ UART_HandleTypeDef huart2;
 
 uint32_t ADCData[2]={0}; // store information from ADC
 
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -307,22 +308,13 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
-void InputValueONETime(uint32_t X){
-
-}
-
 // Toggle LED with interrupt
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){	// Callback = receive interrupt / GPIO_Pin = has 1 pin that assign when press
 												// EXTI = external interrupt(don't interested in main function)
-	static uint32_t timeStamp = 0;
-	static uint32_t timeDelay = 0;
 
 	if (GPIO_Pin == GPIO_PIN_13){
 		HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
-		if (HAL_GetTick() - timeStamp >= timeDelay){
-			timeStamp = HAL_GetTick();
-			timeDelay = 1000+((22695477 * ADCData[0]) + ADCData[1])%10000;
-			HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+
 		}
 	}
 }
